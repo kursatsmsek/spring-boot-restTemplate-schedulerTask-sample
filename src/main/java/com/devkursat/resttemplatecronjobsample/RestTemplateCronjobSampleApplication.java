fa7ff8b5600ad3302878cfa0cbb8ee726b8a6f9a
kursatsmsek <kursatsimsek@protonmail.ch>
2023-11-23 17:35:01 +0300

    initial commit

---
+++ src/main/java/com/devkursat/resttemplatecronjobsample/RestTemplateCronjobSampleApplication.java
@@ -0,0 +1,13 @@
+package com.devkursat.resttemplatecronjobsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateCronjobSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateCronjobSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devkursat/resttemplatecronjobsample/RestTemplateCronjobSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkursat.resttemplatecronjobsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateCronjobSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
